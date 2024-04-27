bba6790c03ed6980dee15aec4b89ad5b0ff07c0e
Sheila3557 <160981799+Sheila3557@users.noreply.github.com>
2024-04-27 13:07:21 -0700

    Update Registration.java

--- src/main/java/org/example/Registration.java
+++ src/main/java/org/example/Registration.java
@@ -1,27 +1,4 @@
-package org.example;
 
-public class Registration {
-    private String email;
-    private String userName;
-    private String password;
-
-    private void displayUserName(){
-        System.out.println(userName);
-    }
-    public void printName(){
-        System.out.println(email);
-    }
-
-    public static void main(String[] args) {
-        Registration reg =new Registration();
-        reg.userName= "Buttons123";
-        reg.password= "pass123";
-        reg.displayUserName();
-        reg.email= "spvgn123@yahoo.com";
-        reg.printName();
-
-    }
-}
 
 
 
